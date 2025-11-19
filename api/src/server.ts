aae285105863072c6ececa81791c1a284c00f7be
Andrew Niccolo Pangilinan <anspangilinan@gmail.com>
2025-11-19 15:51:23 +0800

    [fix] - apply cors plugin on api

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import agentRoutes from "./routes/agent";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/agents", agentRoutes);
